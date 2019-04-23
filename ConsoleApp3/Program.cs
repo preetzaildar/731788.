cb195a2bc5dcdaf5d56cd00c2ff6038764a730cf
Damanpreet Singh <c0731788@mylambton.ca>
2019-04-22 22:10:30 -0400

    commit

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -2,14 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
-namespace ConsoleApp3
+namespace consoleapp3
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Peter Rules the IT Universe!");
+            Download();
+            Console.ReadLine();
+        }
+
+        static async void Download()
+        {
+            await Network.Download();
+            Console.WriteLine("Download Complete");
+        }
+        class Network
+        {
+            static public Task Download()
+            {
+                return Task.Run(() => Thread.Sleep(3000));
+            }
         }
     }
 }
